be0d3be7240be02fbf84ab6d6f9a9cf60ec4330a
PhilipHws <philipherold34@gmail.com>
2026-07-06 11:15:14 +0200

    Add files via upload

--- Projekt/sketch.js
+++ Projekt/sketch.js
@@ -1,10 +1,29 @@
 let lange = 50;
+const farbe = {
+    red:[255, 48, 48]
+}
+
+
+const SHAPES = {
+  I:[[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],
+  O:[[1,1],[1,1]],
+  T:[[0,1,0],[1,1,1],[0,0,0]],
+  S:[[0,1,1],[1,1,0],[0,0,0]],
+  Z:[[1,1,0],[0,1,1],[0,0,0]],
+  J:[[1,0,0],[1,1,1],[0,0,0]],
+  L:[[0,0,1],[1,1,1],[0,0,0]],
+};
+const TYPES = Object.keys(SHAPES);
 
 function setup() {
   createCanvas(720, 1002);
 }
 
-function draw() {
+function bloecke(){
+    square()
+}
+
+function draw() { // Kordinatensystem
   background(220);
   fill(145, 145, 145)
   for(let x = 0; x < 10; x++){
@@ -12,4 +31,8 @@ function draw() {
         square(x*lange+1, y*lange+1, lange)
     }
   }
+ 
+function grid(){
+    
+}
 }
